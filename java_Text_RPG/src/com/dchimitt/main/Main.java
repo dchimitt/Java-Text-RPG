500e56fb375a6f12e682f07be9e335da3c976ecd
dchimitt <chimittd@gmail.com>
2024-07-22 17:31:22 -0500

    Test commit

--- java_Text_RPG/src/com/dchimitt/main/Main.java
+++ java_Text_RPG/src/com/dchimitt/main/Main.java
@@ -2,7 +2,7 @@ package com.dchimitt.main;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Testing committ change.");
 	}
 
 }
