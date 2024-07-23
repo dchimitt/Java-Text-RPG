571580c50f7f9c092a327c26d138bf7c8a2f1d94
dchimitt <chimittd@gmail.com>
2024-07-23 14:14:03 -0500

    In GameLogic class, added userInput method to read user input, with try/catch to handle errors

--- java_Text_RPG/src/com/dchimitt/main/GameLogic.java
+++ java_Text_RPG/src/com/dchimitt/main/GameLogic.java
@@ -1,5 +1,24 @@
 package com.dchimitt.main;
+import java.util.Scanner;
 
+// will never create object of this class, so everything here is static
 public class GameLogic {
-
+	static Scanner in = new Scanner(System.in);
+	
+	// method to get user input 
+	public static int userInput(String prompt, int userChoices) {
+		int input;
+		do {
+			System.out.println(prompt);
+			try {
+				input = in.nextInt();				
+			}
+			catch(Exception e) {
+				input = -1;
+				System.out.println("That is not an integer. Please enter an integer: ");
+			}
+		}
+		while (input < 1 || input > userChoices);
+		return input;
+	}
 }
