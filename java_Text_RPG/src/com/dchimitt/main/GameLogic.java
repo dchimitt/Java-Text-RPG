1a2869659fe53106de6b13b357225e9b6f7f7c24
dchimitt <chimittd@gmail.com>
2024-07-23 16:37:30 -0500

    Added startGame method
    Prints title of game, allows player to pick character name, starts game
    loop

--- java_Text_RPG/src/com/dchimitt/main/GameLogic.java
+++ java_Text_RPG/src/com/dchimitt/main/GameLogic.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 // will never create object of this class, so everything here is static
 public class GameLogic {
 	static Scanner in = new Scanner(System.in);
+	static Player player;
 	
 	// method to get user input 
 	public static int userInput(String prompt, int userChoices) {
@@ -17,8 +18,7 @@ public class GameLogic {
 				input = -1;
 				System.out.println("That is not an integer. Please enter an integer: ");
 			}
-		}
-		while (input < 1 || input > userChoices);
+		} while (input < 1 || input > userChoices);
 		return input;
 	}
 	
@@ -42,5 +42,41 @@ public class GameLogic {
 	public static void typeToContinue() {
 		System.out.println("\nPress any key and hit enter to continue.");
 		in.next(); // this will take the next token entered
+	}
+	
+	public static void startGame() {
+		boolean pickedName = false;
+		String name; 
+		
+		// print title
+		clearConsole();
+		printSeperator(40);
+		printSeperator(30);
+		System.out.println("TALES OF SORROW");
+		System.out.println("TEXT RPG BY DANIEL CHIMITT");
+		printSeperator(30);
+		printSeperator(40);
+		typeToContinue();
+		
+		// allow player to choose name
+		do {
+			clearConsole();
+			printHeader("What is your name, brave adventurer?");
+			name = in.next();
+			
+			clearConsole();
+			printHeader("Your name is " + name + ".\nIs that correct?");
+			System.out.println("(1) Yes!");
+			System.out.println("(2) No, let me change it.");
+			int input = userInput("--> ", 2);
+			if (input == 1)
+				pickedName = true;
+		} while (!pickedName);
+		
+		// create new player object
+		player = new Player(name);
+		
+		// start main game loop
+		// gameLoop();
 	}
 }
