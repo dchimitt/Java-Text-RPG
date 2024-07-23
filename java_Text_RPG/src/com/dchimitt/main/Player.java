c7a3a7756113f4cd94b164d52ed41156cda9606b
dchimitt <chimittd@gmail.com>
2024-07-23 16:18:22 -0500

    Added chooseUpgrade method, allow player to choose 2 traits on creation
    TO-DO:
    a) come up with names for abilities, off/def magic
    b) implement abstract methods
    c) in chooseUpgrade, allow player to revert choice and add error
    handling

--- java_Text_RPG/src/com/dchimitt/main/Player.java
+++ java_Text_RPG/src/com/dchimitt/main/Player.java
@@ -1,20 +1,35 @@
 package com.dchimitt.main;
+import java.util.Scanner;
 
 // NOTE: @Override means inherited methods from abstract superclass differ
 public class Player extends Character {
 	
-	// progressive skills that player can select in attack, magic, and defense
-	public String[] atkUpgrades = {};
+	public static Scanner in = new Scanner(System.in);
+	
+	// progressive skills that player can select in attack, abilities, magic, and defense
+	public String[] atkUpgrades = {"Feeble", "Weak", "Novice", "Developed", "Competent", "Skilled", "Proficient", "Formidable", "Strong", "Mighty", "Powerful", "Herculean"};
+	public String[] defUpgrades = {"Fragile", "Vulnerable", "Squishy", "Soft", "Resilient", "Sturdy", "Durable", "Tough", "Hardy", "Fortified", "Impenetrable", "Invincible"};
 	public String[] abilityUpgrades = {};
 	public String[] offensiveMagUpgrades = {};
 	public String[] defensiveMagUpgrades = {};
-	public String[] defUpgrades = {};
+	
+	// variables to store number of skills added to each of the above options
+	public int numAtkUpgrades, numAbilityUpgrades, numOffensiveMagUpgrades, numDefensiveMagUpgrades, numDefUpgrades;
 	
 	// Player constructor
 	public Player (String name) {
 		// super keyword to use the constructor from superclass
         // starting maximumHp is 100 and exp is 0
 		super(name, 100, 15, 0); 
+		this.numAtkUpgrades = 0;
+		this.numAbilityUpgrades = 0;
+		this.numOffensiveMagUpgrades = 0;
+		this.numDefensiveMagUpgrades = 0;
+		this.numDefUpgrades = 0;
+		// allow player to choose two upgrades at the start of game
+		chooseUpgrade();
+		System.out.println("Please choose another upgrade: ");
+		chooseUpgrade();
 	}
 
 	@Override
@@ -35,4 +50,47 @@ public class Player extends Character {
 		return 0;
 	}
 	
+	// allows player to upgrade one path
+	public void chooseUpgrade() {
+		GameLogic.clearConsole();
+		GameLogic.printHeader("Choose an upgrade:");
+		System.out.println("(1) " + atkUpgrades[numAtkUpgrades]);
+		System.out.println("(2) " + defUpgrades[numDefUpgrades]);
+		System.out.println("(3) " + abilityUpgrades[numAbilityUpgrades]);
+		System.out.println("(4) " + offensiveMagUpgrades[numOffensiveMagUpgrades]);
+		System.out.println("(5) " + defensiveMagUpgrades[numDefensiveMagUpgrades]);
+		
+		// obtain input as player's upgrade choice
+		int input;
+		GameLogic.clearConsole();
+		
+		// ensures user input is between 1-5
+		do {
+			input = GameLogic.userInput("-->", 5);
+			if (input < 1 || input > 5) 
+				System.out.println("Invalid input. Please choose an integer from 1 to 5.");
+		} while (input < 1 || input > 5);
+		
+		// TO-DO: allow player to say wrong choice and add error-handling for choices other than Y/y, N/n
+		if (input == 1) {
+			GameLogic.printHeader("You chose " + atkUpgrades[numAtkUpgrades] + "!");
+			numAtkUpgrades++;			
+		}
+		else if (input == 2) {
+			GameLogic.printHeader("You chose " + defUpgrades[numDefUpgrades] + "!");
+			numDefUpgrades++;
+		}
+		else if (input == 3) {
+			GameLogic.printHeader("You chose " + abilityUpgrades[numAbilityUpgrades] + "!");
+			numAbilityUpgrades++;
+		}
+		else if (input == 4) {
+			GameLogic.printHeader("You chose " + offensiveMagUpgrades[numOffensiveMagUpgrades] + "!");
+			numOffensiveMagUpgrades++;
+		}
+		else {
+			GameLogic.printHeader("You chose " + defUpgrades[numDefUpgrades] + "!");
+			numDefUpgrades++;
+		}
+	}
 }
