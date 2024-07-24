63869f442289d5b3ca2e9bb438aa6f4353affc6a
dchimitt <chimittd@gmail.com>
2024-07-24 13:35:16 -0500

    Added various stats to character superclass
    Added str, dex, int, level, current exp / exp to level up

--- java_Text_RPG/src/com/dchimitt/main/Character.java
+++ java_Text_RPG/src/com/dchimitt/main/Character.java
@@ -3,16 +3,21 @@ package com.dchimitt.main;
 public abstract class Character {
 	// attributes that all players, enemies, and bosses must have
 	public String name;
-	public int maximumHp, currentHp, maximumMana, currentMana, exp;
+	public int strength, dexterity, intelligence, maximumHp, currentHp, maximumMana, currentMana, level, currentExp, expToLevel;
 	
 	// Character constructor
-	public Character(String name, int maximumHp, int maximumMana, int exp) {
+	public Character(String name, int strength, int dexterity, int intelligence, int maximumHp, int maximumMana, int level, int currentExp, int expToLevel) {
 		this.name = name;
+		this.strength = strength;
+		this.dexterity = dexterity;
+		this.intelligence = intelligence;
 		this.maximumHp = maximumHp;
 		this.currentHp = maximumHp;
 		this.maximumMana = maximumMana;
 		this.currentMana = maximumMana;
-		this.exp = exp;
+		this.level = level;
+		this.currentExp = currentExp;
+		this.expToLevel = expToLevel;
 	}
 	
 	// abstract methods
