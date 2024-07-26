6f316cade2a3b8b38d1267d308542e7136857223
dchimitt <chimittd@gmail.com>
2024-07-26 01:54:59 -0500

    Creation of Thing superclass used for making objects

---
+++ java_Text_RPG/src/com/dchimitt/main/Thing.java
@@ -0,0 +1,29 @@
+package com.dchimitt.main;
+
+public class Thing {
+	// superclass Thing to extend to all objects used
+	
+	private String name;
+	private String description;
+	
+	public Thing(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
