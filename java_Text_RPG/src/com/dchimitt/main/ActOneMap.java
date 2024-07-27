ddeb157c5835ddbaaea7963cc6d3500d5b910b7f
dchimitt <chimittd@gmail.com>
2024-07-27 17:14:46 -0500

    Updated methods w/ new names

--- java_Text_RPG/src/com/dchimitt/main/ActOneMap.java
+++ java_Text_RPG/src/com/dchimitt/main/ActOneMap.java
@@ -13,26 +13,19 @@ public class ActOneMap {
 		public static final int NOEXIT = -1;
 	};
 	
-	private int currentPlayerPosition;
+	private int currentPlayerPosition = 0;
 	private static ArrayList <Room> actOneMap;
 	
 	List<String> input = new ArrayList<>(Arrays.asList("n", "s", "e", "w"));
 	
 	public static void initializeActOneMap() {
-		if (actOneMap == null) {
-			actOneMap = new ArrayList<Room>();
+		actOneMap = new ArrayList<Room>();
 		
-			// add rooms to the act one map
-			actOneMap.add(new Room("Placeholder1", "description1", Direction.NOEXIT, 2, 1, Direction.NOEXIT));
-			actOneMap.add(new Room("Placeholder2", "description2", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, 0));
-			actOneMap.add(new Room("Placeholder3", "description3", 0, Direction.NOEXIT, 3, Direction.NOEXIT));
-			actOneMap.add(new Room("Placeholder4", "description4", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, 2));
-		}
-	}
-	
-	public ActOneMap() {
-		initializeActOneMap();
-		currentPlayerPosition = 0;
+		// add rooms to the act one map
+		actOneMap.add(new Room("Placeholder1", "description1", Direction.NOEXIT, 2, 1, Direction.NOEXIT));
+		actOneMap.add(new Room("Placeholder2", "description2", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, 0));
+		actOneMap.add(new Room("Placeholder3", "description3", 0, Direction.NOEXIT, 3, Direction.NOEXIT));
+		actOneMap.add(new Room("Placeholder4", "description4", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, 2));
 	}
 		
 	public static Room getActOneMapStartingRoom()
@@ -46,16 +39,16 @@ public class ActOneMap {
 		
 		switch (direction) {
 		case NORTH:
-			newPlayerPosition = currentRoom.getN();
+			newPlayerPosition = currentRoom.getNorth();
 			break;
 		case SOUTH:
-			newPlayerPosition = currentRoom.getS();
+			newPlayerPosition = currentRoom.getSouth();
 			break;
 		case EAST:
-			newPlayerPosition = currentRoom.getE();
+			newPlayerPosition = currentRoom.getEast();
 			break;
 		case WEST:
-			newPlayerPosition = currentRoom.getW();
+			newPlayerPosition = currentRoom.getWest();
 			break;			
 		}
 		
