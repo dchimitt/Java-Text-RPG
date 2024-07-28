27001be5c943e8ccafcb7b9cbadcd27c88851a3d
dchimitt <chimittd@gmail.com>
2024-07-28 14:30:13 -0500

    Added comments to show room indices for actOneMap

---
+++ java_Text_RPG/src/com/dchimitt/main/ActOneMap.java
@@ -0,0 +1,60 @@
+package com.dchimitt.main;
+import java.util.ArrayList;
+
+public class ActOneMap {
+	
+	public enum Direction {
+		NORTH, 
+		SOUTH, 
+		EAST,  
+		WEST; 
+		
+		// named variable so not confused with -1 later
+		public static final int NOEXIT = -1;
+	};
+	
+	private int currentPlayerPosition = 0;
+	private ArrayList <Room> actOneMap;
+	
+	public ActOneMap() {
+		actOneMap = new ArrayList<Room>();
+		
+		// Adding rooms to the ArrayList. Integers indicate the indices of the connecting rooms in directions of order (North, South, East, West)
+		actOneMap.add(new Room("Placeholder1", "description1", Direction.NOEXIT, 2, 1, Direction.NOEXIT));                    // 0
+		actOneMap.add(new Room("Placeholder2", "description2", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, 0));     // 1
+		actOneMap.add(new Room("Placeholder3", "description3", 0, Direction.NOEXIT, 3, Direction.NOEXIT));                    // 2
+		actOneMap.add(new Room("Placeholder4", "description4", Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT, 2));     // 3
+	}
+	
+	public Room getActOneMapStartingRoom()
+	{
+		return actOneMap.get(0);
+	}
+	
+	public int movePlayerTo(Direction direction) {
+		Room currentRoom = actOneMap.get(currentPlayerPosition);
+		int newPlayerPosition = Direction.NOEXIT; // default set to -1 to handle invalid moves
+		
+		switch (direction) {
+		case NORTH:
+			newPlayerPosition = currentRoom.getNorth();
+			break;
+		case SOUTH:
+			newPlayerPosition = currentRoom.getSouth();
+			break;
+		case EAST:
+			newPlayerPosition = currentRoom.getEast();
+			break;
+		case WEST:
+			newPlayerPosition = currentRoom.getWest();
+			break;			
+		}
+		
+		if (newPlayerPosition != Direction.NOEXIT) 
+			currentPlayerPosition = newPlayerPosition;
+		else
+			System.out.println("You cannot move in that direction!");
+		
+		return currentPlayerPosition;
+	}
+}
