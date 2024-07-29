0a970830fc16c650fccc6fe53162d9d7b7ee8e11
dchimitt <chimittd@gmail.com>
2024-07-29 14:55:04 -0500

    Added new enemy class

--- java_Text_RPG/src/actOneEnemies/ReizartBat.java
+++ java_Text_RPG/src/actOneEnemies/ReizartBat.java
@@ -1,6 +1,37 @@
 package actOneEnemies;
 import com.dchimitt.main.Character;
+import com.dchimitt.main.Room;
 
-public class ReizartBat {
+public class ReizartBat extends Character {
+
+	public ReizartBat(String name, int strength, int dexterity, int intelligence, int maximumHp, int maximumMana,
+			int level, Room currentRoomInAct) {
+		super(name, strength, dexterity, intelligence, maximumHp, maximumMana, level, currentRoomInAct);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int attack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int useAbility() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int useMagic() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int defend() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
