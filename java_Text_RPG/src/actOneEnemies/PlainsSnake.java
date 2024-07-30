af705161483fc330bf6ff5b6f533329531421f0a
dchimitt <chimittd@gmail.com>
2024-07-30 15:47:10 -0500

    Added new plains monster (snake) class

---
+++ java_Text_RPG/src/actOneEnemies/PlainsSnake.java
@@ -0,0 +1,34 @@
+package actOneEnemies;
+
+import com.dchimitt.main.Character;
+
+public class PlainsSnake extends Character implements java.io.Serializable {
+
+	public PlainsSnake(String name, int currentAct) {
+		super(name, 1, 1, 1, 10, 0, 2);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int attack() {
+		return (int) (Math.random() * (level / 2 + strength * 2) + dexterity * 2);
+	}
+
+	@Override
+	public int useAbility() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int useMagic() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int defend() {
+		return (int) (Math.random() * (level / 2 + strength * 2) + dexterity * 2);
+	}
+
+}
