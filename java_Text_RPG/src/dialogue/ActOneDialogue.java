f4567e53e5fb04bc41076e7fb8b6cbb05896b254
dchimitt <chimittd@gmail.com>
2024-07-30 16:39:22 -0500

    Added dialogue for first boss fight

--- java_Text_RPG/src/dialogue/ActOneDialogue.java
+++ java_Text_RPG/src/dialogue/ActOneDialogue.java
@@ -7,4 +7,12 @@ public class ActOneDialogue implements java.io.Serializable {
 		System.out.println("PLACEHOLDER HI SON");
 		GameLogic.typeToContinue();
 	}
+	
+	public static void fightThraxxDialogue() {
+		System.out.println("You reach the exit of the cave, a sigh of relief on your face.");
+		System.out.println("Walking toward the exit, a shadow appears out of the corner of your eye.");
+		System.out.println("A hideous troll, equipped with a stone hammer and at least three times your size, snorts and scowls.");
+		System.out.println("You plant your feet, ready to challenge the massive beast before you...");
+		System.out.println("The troll yells, \"ME SMORC ME SMASH TINY HOOMAN!!!\" and charges you");
+	}
 }
