2abca23e3af12235d785887ada5a4e447aefc633
dchimitt <chimittd@gmail.com>
2024-07-30 15:45:51 -0500

    Added method to create plains goblin

--- java_Text_RPG/src/actOneEnemies/ActOneEnemyCreation.java
+++ java_Text_RPG/src/actOneEnemies/ActOneEnemyCreation.java
@@ -1,14 +1,20 @@
 package actOneEnemies;
 
+// enemies created in order of areas traversed in game
 public class ActOneEnemyCreation {
+	
+	// Reizart plains monsters
+	public PlainsGoblin createPlainsGoblin() {
+		return new PlainsGoblin("Plains Goblin", 1);
+	}
+	
+	// Reizart cave monsters
 	public CaveBat createCaveBat() {
 		return new CaveBat("Cave Bat", 2);
 	}
-	
 	public CaveSpider createCaveSpider() {
 		return new CaveSpider("Cave Spider", 2);
 	}
-	
 	public CaveSlug createCaveSlug() {
 		return new CaveSlug("Cave Slug", 2);
 	}
