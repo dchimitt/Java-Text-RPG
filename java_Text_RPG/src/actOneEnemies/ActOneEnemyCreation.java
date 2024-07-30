a2519b9549813d09bff02c83f967bf61d5fe55cd
dchimitt <chimittd@gmail.com>
2024-07-30 15:38:53 -0500

    Added method to create cave spider

--- java_Text_RPG/src/actOneEnemies/ActOneEnemyCreation.java
+++ java_Text_RPG/src/actOneEnemies/ActOneEnemyCreation.java
@@ -4,4 +4,8 @@ public class ActOneEnemyCreation {
 	public CaveBat createCaveBat() {
 		return new CaveBat("Cave Bat", 2);
 	}
+	
+	public CaveSpider createCaveSpider() {
+		return new CaveSpider("Cave Spider", 2);
+	}
 }
