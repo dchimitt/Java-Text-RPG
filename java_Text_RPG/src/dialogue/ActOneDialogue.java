ffd0599fd6a8a2cf548ce0547c9e6bc6bdc85c21
dchimitt <chimittd@gmail.com>
2024-07-30 16:53:40 -0500

    Added typeToContinue so boss dialogue is shown to player

--- java_Text_RPG/src/dialogue/ActOneDialogue.java
+++ java_Text_RPG/src/dialogue/ActOneDialogue.java
@@ -14,5 +14,6 @@ public class ActOneDialogue implements java.io.Serializable {
 		System.out.println("A hideous troll, equipped with a stone hammer and at least three times your size, snorts and scowls.");
 		System.out.println("You plant your feet, ready to challenge the massive beast before you...");
 		System.out.println("The troll yells, \"ME SMORC ME SMASH TINY HOOMAN!!!\" and charges you");
+		GameLogic.typeToContinue();
 	}
 }
