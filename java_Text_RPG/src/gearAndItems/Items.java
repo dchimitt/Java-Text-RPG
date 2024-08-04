14e495da1b3937f77d9d13174e4b5b0808ddf172
dchimitt <chimittd@gmail.com>
2024-08-04 14:57:09 -0500

    Created basic template for PlayerItems class

--- java_Text_RPG/src/gearAndItems/Items.java
+++
@@ -1,5 +0,0 @@
-package gearAndItems;
-
-public class Items {
-
-}
---
+++ java_Text_RPG/src/gearAndItems/PlayerItems.java
@@ -0,0 +1,85 @@
+package gearAndItems;
+
+public class PlayerItems {
+	public static enum Items {
+		WEAK_HEALING_POTION("Weak Healing Potion", "Placeholder", 0),
+		HEALING_POTION("Healing Potion", "Placeholder", 0),
+		INFUSED_HEALING_POTION("Infused Healing Potion", "Placeholder", 0),
+		WEAK_MANA_POTION("Weak Mana Potion", "Placeholder", 0),
+		MANA_POTION("Mana Potion", "Placeholder", 0),
+		INFUSED_MANA_POTION("Infused Mana Potion", "Placeholder", 0),
+		ANTIDOTE("Weak Healing Potion", "Placeholder", 0),
+		REVIVE("Weak Healing Potion", "Placeholder", 0);
+	
+		// instance variables for each ability
+		private final String itemName;
+		private final String itemDescription;
+		private int itemQuantity;
+		
+		// Constructor
+		Items(String itemName, String itemDescription, int itemQuantity) {
+			this.itemName = itemName;
+			this.itemDescription = itemDescription;
+			this.itemQuantity = itemQuantity;
+		}
+		
+		// getter methods
+		public String getItemName() {
+			return itemName;
+		}		
+		public String getItemDescription() {
+			return itemDescription;
+		}
+		public int getItemQuantity() {
+			return itemQuantity;
+		}
+		
+		// methods to increase item quantity when player makes a purchase
+		public void purchaseWeakHealingPotion() {
+			
+		}
+		public void purchaseHealingPotion() {
+			
+		}
+		public void purchaseInfusedHealingPotion() {
+			
+		}
+		public void purchaseWeakManaPotion() {
+			
+		}
+		public void purchaseManaPotion() {
+			
+		}
+		public void purchaseInfusedManaPotion() {
+			
+		}
+		public void purchaseAntidote() {
+			
+		}
+		public void purchaseRevive() {
+			
+		}
+		
+		// methods to use healing potions
+		public void useWeakHealingPotion() {
+			
+		}
+		public void useHealingPotion() {
+			
+		}
+		public void useInfusedHealingPotion() {
+			
+		}
+		
+		// methods to use mana potions
+		public void useWeakManaPotion() {
+			
+		}
+		public void useManaPotion() {
+			
+		}
+		public void useInfusedManaPotion() {
+			
+		}		
+	}
+}
