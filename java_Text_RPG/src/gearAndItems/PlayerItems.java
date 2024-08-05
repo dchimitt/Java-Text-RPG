81eafd37c54fbabcad2aab34302ca81e96867cd3
dchimitt <chimittd@gmail.com>
2024-08-05 14:59:13 -0500

    Added functionality to potion item consumption (placeholder values)

--- java_Text_RPG/src/gearAndItems/PlayerItems.java
+++ java_Text_RPG/src/gearAndItems/PlayerItems.java
@@ -1,5 +1,7 @@
 package gearAndItems;
 
+import com.dchimitt.main.GameLogic;
+
 public class PlayerItems {
 	public static enum Items {
 		WEAK_HEALING_POTION("Weak Healing Potion", "Placeholder", 0),
@@ -36,58 +38,64 @@ public class PlayerItems {
 		
 		// methods to increase item quantity when player makes a purchase or obtains as drop from monster/boss
 		public void increaseWeakHealingPotionQuantity() {
-			
+			Items.WEAK_HEALING_POTION.itemQuantity++;
 		}
 		public void increaseHealingPotionQuantity() {
-			
+			Items.HEALING_POTION.itemQuantity++;
 		}
 		public void increaseInfusedHealingPotionQuantity() {
-			
+			Items.INFUSED_HEALING_POTION.itemQuantity++;
 		}
 		public void increaseWeakManaPotionQuantity() {
-			
+			Items.WEAK_MANA_POTION.itemQuantity++;
 		}
 		public void increaseManaPotionQuantity() {
-			
+			Items.MANA_POTION.itemQuantity++;
 		}
 		public void increaseInfusedManaPotionQuantity() {
-			
+			Items.INFUSED_HEALING_POTION.itemQuantity++;
 		}
 		public void increaseAntidoteQuantity() {
-			
+			Items.ANTIDOTE.itemQuantity++;
 		}
-		public void increaseReviveQuantity() {
-			
+		public void increaseReapersBaneQuantity() {
+			Items.REAPERS_BANE.itemQuantity++;
 		}
 		
 		// methods to use healing potions
 		public void useWeakHealingPotion() {
-			
+			GameLogic.player.currentHp = GameLogic.player.currentHp + 10;
+			Items.WEAK_HEALING_POTION.itemQuantity--;
 		}
 		public void useHealingPotion() {
-			
+			GameLogic.player.currentHp = GameLogic.player.currentHp + 25;
+			Items.HEALING_POTION.itemQuantity--;
 		}
 		public void useInfusedHealingPotion() {
-			
+			GameLogic.player.currentHp = GameLogic.player.currentHp + 50;
+			Items.INFUSED_HEALING_POTION.itemQuantity--;
 		}
 		
 		// methods to use mana potions
 		public void useWeakManaPotion() {
-			
+			GameLogic.player.currentMana = GameLogic.player.currentMana + 5;
+			Items.WEAK_MANA_POTION.itemQuantity--;
 		}
 		public void useManaPotion() {
-			
+			GameLogic.player.currentMana = GameLogic.player.currentMana + 15;
+			Items.MANA_POTION.itemQuantity--;
 		}
 		public void useInfusedManaPotion() {
-			
+			GameLogic.player.currentMana = GameLogic.player.currentMana + 30;
+			Items.INFUSED_MANA_POTION.itemQuantity--;
 		}		
 		
 		// methods to use other items
 		public void useAntidote() {
-			
+			Items.ANTIDOTE.itemQuantity--;
 		}
 		public void useReapersBane() {
-			
+			Items.REAPERS_BANE.itemQuantity--;
 		}
 	}
 }
