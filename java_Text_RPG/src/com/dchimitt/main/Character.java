f24cbf40e0848e1bf928466ca2b11703b9ebd5ed
dchimitt <chimittd@gmail.com>
2024-08-30 12:21:22 -0500

    Move comment to more accurately reflect variables in Char superclass

--- java_Text_RPG/src/com/dchimitt/main/Character.java
+++ java_Text_RPG/src/com/dchimitt/main/Character.java
@@ -3,12 +3,13 @@ package com.dchimitt.main;
 import java.io.Serializable;
 
 public abstract class Character implements Serializable {
-	// attributes that all players, enemies, and bosses must have
+	// all possible future attributes that players/enemies may or may not have
 	public String name;
 	public int strength, dexterity, intelligence, maximumHp, currentHp, maximumMana, currentMana, level, currentExp, expToLevel, gold, currentAct, playerMovementCounter;
 	public Room currentRoomInAct;
 	
 	// Character constructor
+	// attributes that all players, enemies, and bosses must have
 	public Character(String name, int strength, int dexterity, int intelligence, int maximumHp, int maximumMana, int level) {
 		this.name = name;
 		this.strength = strength;
